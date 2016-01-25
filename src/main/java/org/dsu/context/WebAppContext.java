b9e7b031f829f33610066a5d2cc8a7ae021fc5db
dsurogatov <dsurogatov@gmail.com>
2016-01-25 20:24:18 +0300

    move the spring web to the java config

---
+++ src/main/java/org/dsu/context/WebAppContext.java
@@ -0,0 +1,51 @@
+package org.dsu.context;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("org.dsu")
+@EnableAspectJAutoProxy
+public class WebAppContext extends WebMvcConfigurerAdapter {
+ 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+    }
+ 
+    @Bean
+    public UrlBasedViewResolver setupViewResolver() {
+        UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+        
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+ 
+        return resolver;
+    }
+    
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+ 
+        messageSource.setBasename("i18n/messages");
+        messageSource.setUseCodeAsDefaultMessage(true);
+ 
+        return messageSource;
+    }
+    
+    @Bean
+    public JpaContext jpaContext() {
+    	return new JpaContext();
+    }
+}
